a405a3a9137ac8d88c81cd051de8a84079abf614
dkje <developer.kje@gmail.com>
2020-08-07 09:14:01 +0900

    [lesson2]complie ts to js

---
+++ sandbox.js
@@ -0,0 +1,13 @@
+var charactor = 'luigi??';
+console.log(charactor);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
+// ts는 js 파일로 컴파일해 사용해야한다
+// terminal로 ts 컴파일하기
+// tsc 파일명.ts 파일명.js
+// js 파일명을 생략하면 같은 이름으로 컴파일함
+// tsc 파일명.ts -w
+// watch 옵션을 주면 수정시 자동으로 컴파일함

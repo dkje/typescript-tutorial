8f8bcecfdc985fa561fbcacedd36435b9c1acd77
dkje <developer.kje@gmail.com>
2020-08-07 10:18:35 +0900

    [lesson5]명시적 타입선언

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,41 @@
+//explicit types 명시적 타입
+var character;
+var age;
+var isLoggedIn;
+// age ='30';
+age = 30;
+// isLoggedIn =20;
+isLoggedIn = true;
 //arrays
-// 배열 생성시 초기값으로 고정 타입을 정한다.
-var names = ['name1', 'name2', 'name3'];
-names.push('toad');
-// names.push(3);
-// names[0] = 3;
-var numbers = [10, 20, 30, 40];
-numbers.push(50);
-// numbers.push('why');
-// numbers[1] = 'shaun';
-// 여러가지 타입을 넣어 초기화 했을 경우는 모든 타입이 들어갈 수 있다.
-var mixed = ['ken', 4, 'chun-li', 8, 9, true];
-mixed.push(true);
-// 객체의 속성 값도 초기값에 따른 고정 타입을 갖는다
-var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-};
-ninja.age = 40;
-ninja.name = 'tyw';
-// ninja.age = 'tesd';
-// 새 속성 할당 가능
-// ninja.skills = ['fighting', 'sneaking'];
-// 객체를 재정의할때 기존의 속성값을 / 속성값만 모두 갖고있지 않으면 오류 발생
-ninja = {
-    name: 'yoshi',
-    belt: 'orange',
-    age: 40
+var ninjas;
+// 고정 타입 외의 값을 넣을 수 없다
+// ninjas = [10, 20];
+ninjas = ['yoshi', 'mario'];
+ninjas.push('ok?');
+console.log(ninjas);
+// let blankArr: string[];
+// 빈 배열로 초기화 되지 않으면 push할 수 없다
+// blankArr.push('1234');
+var blankArr = [];
+blankArr.push('1234');
+console.log(blankArr);
+//union types 여러타입 사용하기
+var mixed = [];
+mixed.push('hello');
+mixed.push(30);
+mixed.push(false);
+console.log(mixed);
+var uid;
+uid = '123';
+uid = 123;
+// uid = false;
+//objects
+var ninjaOne;
+ninjaOne = { name: 'yoshi', age: 30 };
+ninjaOne = ['yoshi', 30];
+var ninjaTwo;
+ninjaTwo = {
+    name: '123',
+    age: 30,
+    beltColour: 'blue'
 };

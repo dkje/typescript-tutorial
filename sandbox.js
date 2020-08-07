227cb42bea252353fa19f03f08bef1476f1e98b8
dkje <developer.kje@gmail.com>
2020-08-07 09:29:14 +0900

    [lesson3]타입과 함께 변수 생성하기 & 신택스 에러

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,23 @@
-var charactor = 'luigi??';
-console.log(charactor);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+////////// lesson2
 // ts는 js 파일로 컴파일해 사용해야한다
 // terminal로 ts 컴파일하기
 // tsc 파일명.ts 파일명.js
 // js 파일명을 생략하면 같은 이름으로 컴파일함
 // tsc 파일명.ts -w
 // watch 옵션을 주면 수정시 자동으로 컴파일함
+////////// lesson3
+// ts는 변수 타입을 지정 가능하다
+// 타입 에러 발생 시 신텍스 에러로 체크 가능하다
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+// character = 50
+character = 'test';
+//age = 'yoshi';
+age = 50;
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(6.4));
--- sandbox.ts
+++ sandbox.ts
@@ -1,19 +1,27 @@
-const charactor = 'luigi??';
-
-console.log(charactor);
-
-const inputs = document.querySelectorAll('input');
-
-console.log(inputs);
-
-inputs.forEach((input) => {
-    console.log(input);
-});
-
+////////// lesson2
 // ts는 js 파일로 컴파일해 사용해야한다
 // terminal로 ts 컴파일하기
 // tsc 파일명.ts 파일명.js
 // js 파일명을 생략하면 같은 이름으로 컴파일함
-
 // tsc 파일명.ts -w
 // watch 옵션을 주면 수정시 자동으로 컴파일함
+
+////////// lesson3
+// ts는 변수 타입을 지정 가능하다
+// 타입 에러 발생 시 신텍스 에러로 체크 가능하다
+
+let character: string = 'mario';
+let age: number = 30;
+let isBlackBelt: boolean = false;
+
+// character = 50
+character = 'test';
+//age = 'yoshi';
+age = 50;
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+};
+
+console.log(circ(6.4));

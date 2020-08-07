c40c11c777792394a4480b08537c2f786768fe72
dkje <developer.kje@gmail.com>
2020-08-07 10:38:46 +0900

    [lesson7]tsconfig설정

---
+++ public/sandbox.js
@@ -0,0 +1,8 @@
+"use strict";
+// tsc --init 으로 tsconfig.json 파일 생성
+// rootDir과 outDir 설정
+// 터미널에 tsc만 입력해도 알아서 컴파일함
+// rootDir 밖의 ts파일도 인식해 컴파일하기 때문에
+//tsconfig.json에
+// "include": ["src"]
+// 설정을 반드시 추가해야함

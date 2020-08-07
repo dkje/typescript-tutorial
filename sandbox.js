93c93c4e319cbfc813aceac36a5ee599eac27957
dkje <developer.kje@gmail.com>
2020-08-07 10:28:30 +0900

    [lesson6]다이나믹 타입선언

--- sandbox.js
+++ sandbox.js
@@ -1,41 +1,24 @@
-//explicit types 명시적 타입
-var character;
-var age;
-var isLoggedIn;
-// age ='30';
-age = 30;
-// isLoggedIn =20;
-isLoggedIn = true;
-//arrays
-var ninjas;
-// 고정 타입 외의 값을 넣을 수 없다
-// ninjas = [10, 20];
-ninjas = ['yoshi', 'mario'];
-ninjas.push('ok?');
-console.log(ninjas);
-// let blankArr: string[];
-// 빈 배열로 초기화 되지 않으면 push할 수 없다
-// blankArr.push('1234');
-var blankArr = [];
-blankArr.push('1234');
-console.log(blankArr);
-//union types 여러타입 사용하기
+//Dynamic Types
+var age = 25;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'nameaname' };
+console.log(age);
 var mixed = [];
-mixed.push('hello');
-mixed.push(30);
+mixed.push('123');
+mixed.push(123);
 mixed.push(false);
 console.log(mixed);
-var uid;
-uid = '123';
-uid = 123;
-// uid = false;
-//objects
-var ninjaOne;
-ninjaOne = { name: 'yoshi', age: 30 };
-ninjaOne = ['yoshi', 30];
-var ninjaTwo;
-ninjaTwo = {
-    name: '123',
-    age: 30,
-    beltColour: 'blue'
+var ninja;
+ninja = {
+    name: 'yoshi',
+    age: 45
 };
+console.log(ninja);
+ninja = {
+    name: 123,
+    age: '45'
+};
+console.log(ninja);
--- sandbox.ts
+++ sandbox.ts
@@ -1,53 +1,28 @@
-//explicit types 명시적 타입
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
+//Dynamic Types
 
-// age ='30';
-age = 30;
-// isLoggedIn =20;
-isLoggedIn = true;
+let age: any = 25;
 
-//arrays
-let ninjas: string[];
-// 고정 타입 외의 값을 넣을 수 없다
-// ninjas = [10, 20];
-ninjas = ['yoshi', 'mario'];
-ninjas.push('ok?');
-console.log(ninjas);
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'nameaname' };
+console.log(age);
 
-// let blankArr: string[];
-// 빈 배열로 초기화 되지 않으면 push할 수 없다
-// blankArr.push('1234');
-let blankArr: string[] = [];
-blankArr.push('1234');
-console.log(blankArr);
-
-//union types 여러타입 사용하기
-let mixed: (string | number | boolean)[] = [];
-mixed.push('hello');
-mixed.push(30);
+let mixed: any[] = [];
+mixed.push('123');
+mixed.push(123);
 mixed.push(false);
 console.log(mixed);
 
-let uid: string | number;
-uid = '123';
-uid = 123;
-// uid = false;
-
-//objects
-let ninjaOne: object;
-ninjaOne = { name: 'yoshi', age: 30 };
-ninjaOne = ['yoshi', 30];
-
-let ninjaTwo: {
-    name: string;
-    age: number;
-    beltColour: string;
+let ninja: { name: any; age: any };
+ninja = {
+    name: 'yoshi',
+    age: 45,
 };
-
-ninjaTwo = {
-    name: '123',
-    age: 30,
-    beltColour: 'blue',
+console.log(ninja);
+ninja = {
+    name: 123,
+    age: '45',
 };
+console.log(ninja);
